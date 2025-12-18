12bb6f635df3dc7a38c780af8a78d2fd97a47b33
diana12345670 <dianasantanacarvalho@gmail.com>
2025-12-17 21:49:04 -0300

    simulador 1.7

--- server/static.ts
+++ server/static.ts
@@ -3,17 +3,28 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "../public");
+  // In production: __dirname is dist/ (from compiled index.cjs)
+  // So we need dist/public which is ./public
+  const distPath = path.resolve(__dirname, "./public");
+  
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to run "npm run build" first`,
     );
   }
 
-  app.use(express.static(distPath));
+  // Serve static files with no-cache header
+  app.use(express.static(distPath, {
+    setHeaders: (res) => {
+      res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
+      res.setHeader("Pragma", "no-cache");
+      res.setHeader("Expires", "0");
+    }
+  }));
 
   // Serve index.html for all routes (SPA fallback)
   app.get("*", (_req, res) => {
+    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
